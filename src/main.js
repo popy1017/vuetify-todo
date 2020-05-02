82af259bc7389eef70fbd8f319a50780d7b78775
fkn1017 <kosuke.fukano@gmail.com>
2020-05-02 15:23:05 +0900

    dayjsのインポートと、フィルター作成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "dayjs/locale/ja"
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedFormat from "dayjs/plugin/localizedFormat"
 
+dayjs.extend(relativeTime)
+dayjs.extend(localizedFormat)
+dayjs.locale("ja");
+
+Vue.prototype.$dayjs = dayjs
 Vue.config.productionTip = false
 
+Vue.filter("fromNow", (date) => dayjs().to(date));
+Vue.filter("llll", (date) => dayjs(date).format("llll"));
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
