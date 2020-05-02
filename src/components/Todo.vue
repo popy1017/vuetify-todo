bed0caa5580d6a33203c12de00d5be06c61be9dc
fkn1017 <kosuke.fukano@gmail.com>
2020-05-02 16:04:39 +0900

    不要なdivタグを削除。スタイルを調整

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="item-card border">
-    <input class="checkbox border" type="checkbox">
+    <input class="checkbox border" type="checkbox" />
     <div class="todo_detail border">
-      <h3>{{todo.title}}</h3>
-      <div>
-        <div class="dueDate border">
-          <div v-if="showRelative">{{todo.dueDate | fromNow}}</div>
-          <div v-else>{{todo.dueDate | llll}}</div>
-        </div>
-        <div>{{todo.note}}</div>
-      </div> 
-    </div>   
+      <h3>{{ todo.title }}</h3>
+      <div class="dueDate border">
+        <div v-if="showRelative">{{ todo.dueDate | fromNow }}</div>
+        <div v-else>{{ todo.dueDate | llll }}</div>
+      </div>
+      <div class="note">{{ todo.note }}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: ["todo", "showRelative"],
-}
+};
 </script>
 
 <style scoped>
@@ -31,6 +29,7 @@ export default {
 .checkbox {
   display: inline-block;
   vertical-align: middle;
+  margin: 0 1rem;
 }
 .todo_detail {
   width: 90%;
@@ -41,5 +40,8 @@ export default {
 .dueDate {
   text-align: right;
   height: auto;
+}
+.note {
+  margin-bottom: 1rem;
 }
 </style>
