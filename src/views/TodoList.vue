816f47f1d5134ce4b2278f73a9bb9db04e085ef5
fkn1017 <kosuke.fukano@gmail.com>
2020-05-08 19:38:27 +0900

    簡単な期日順ソート機能を追加

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -16,10 +16,18 @@
       <v-btn @click="toRegisterTodo" color="success" class="font-weight-bold">
         <v-icon left>mdi-pencil-plus</v-icon> 新規作成
       </v-btn>
+      <v-spacer></v-spacer>
+      <v-select
+        v-model="sortMode"
+        :items="items"
+        label="Sort"
+        outlined
+        dense
+      ></v-select>
     </v-row>
     <v-row class="flex-column" justify="center">
       <Todo
-        v-for="todo in filteredTodo"
+        v-for="todo in sortedTodos"
         :key="todo.id"
         :todo="todo"
         :show-relative="showRelative"
@@ -33,26 +41,47 @@ import Todo from "@/components/Todo";
 
 export default {
   name: "TodoList",
+  components: {
+    Todo,
+  },
   data() {
     return {
       hideDoneTodo: true,
       showRelative: true,
+      items: ["期日順", "作成順"],
+      sortMode: "期日順",
     };
   },
   computed: {
-    filteredTodo() {
-      if (this.hideDoneTodo) {
-        return this.$root.todos.filter((item) => item.done == false);
+    sortedTodos() {
+      const todos = this.shownTodos();
+      if (this.sortMode == "期日順") {
+        todos.sort(this.compareDate);
+      } else if (this.sortMode == "作成順") {
+        return todos;
       }
-      return this.$root.todos;
+
+      return todos;
     },
-  },
-  components: {
-    Todo,
   },
   methods: {
     toRegisterTodo() {
       this.$router.push("/create");
+    },
+    shownTodos() {
+      const todos = [...this.$root.todos];
+      if (this.hideDoneTodo) {
+        return todos.filter((item) => item.done == false);
+      }
+      return todos;
+    },
+    compareDate(a, b) {
+      const d1 = this.$root.$dayjs(a.dueDate);
+      const d2 = this.$root.$dayjs(b.dueDate);
+
+      if (d1.isAfter(d2)) return 1;
+      else if (d1.isBefore(d2)) return -1;
+      return 0;
     },
   },
 };
