29ca33d8720163d369214300922f6c8ccd4aaa5e
fkn1017 <kosuke.fukano@gmail.com>
2020-05-15 23:38:40 +0900

    ビルド後のファイルを追加

--- src/libs/localstorage.js
+++ src/libs/localstorage.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import localStorage from "vue-ls";
 
 const options = {
-  namespace: "vue_todo_ls_", // key prefix
+  namespace: "vue_todo_ls_1_", // key prefix
   name: "ls", // name variable Vue.[ls] or this.[$ls],
   storage: "local", // storage name session, local, memory
 };
